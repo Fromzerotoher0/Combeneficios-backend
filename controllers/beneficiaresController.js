736ea79b7160e916221317a92e68235e9a6ee0db
Anderson <ander.er985@gmail.com>
2021-10-01 16:24:42 -0500

    email validation added

--- controllers/beneficiaresController.js
+++ controllers/beneficiaresController.js
@@ -1,10 +1,3 @@
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-const connection = require("../database/db");
-const { promisify } = require("util");
-const { log } = require("console");
-
-//metodo para registrar un usuario
 exports.register = async (req, res) => {
   try {
     //parametros obtenidos del body de la peticion
@@ -20,8 +13,8 @@ exports.register = async (req, res) => {
     const departamento = req.body.departamento;
     const ciudad = req.body.ciudad;
     const imgUrl = `https://combeneficios.herokuapp.com/public/${req.file.filename}`;
+    const titular_id = req.body.titular_id;
 
-    let titular_id = 0;
     let departamento_string = "";
     let hora = new Date().getHours();
     let minuto = new Date().getMinutes();
@@ -53,20 +46,6 @@ exports.register = async (req, res) => {
       });
     } else {
       connection.query(
-        //consulta para obtener el nombre del departamento por medio de su id
-        "SELECT * from users where email = ?",
-        [correo],
-        async (error, results) => {
-          if (results.length > 0) {
-            res.status(400).json({
-              error: "true",
-              msg: "correo electronico en uso",
-            });
-          }
-        }
-      );
-
-      connection.query(
         //consulta para obtener el id del ultimo titular registrado y asignarselo
         "SELECT MAX(id) AS id FROM users",
         async (error, results) => {
@@ -77,7 +56,6 @@ exports.register = async (req, res) => {
           }
         }
       );
-
       connection.query(
         //consulta para obtener el nombre del departamento por medio de su id
         "SELECT departamento from departamentos d where id_departamento = ?",
@@ -141,139 +119,4 @@ exports.register = async (req, res) => {
   } catch (error) {
     console.log(error);
   }
-};
-
-//metodo de autenticacion
-exports.login = async (req, res) => {
-  try {
-    //parametros del body
-    const user = req.body.identificacion;
-    const contrasena = req.body.contrasena;
-
-    connection.query(
-      //buscar al usuario en la bd por su documento
-      "SELECT * FROM users where nro_documento = ?",
-      [user],
-      async (error, results) => {
-        //comparar la contraseña ingresada con la de la bd
-        if (
-          results.length == 0 ||
-          !(await bcrypt.compareSync(contrasena, results[0].contrasena))
-        ) {
-          res.status(400).json({
-            error: true,
-            msg: "usuario/contraseña incorrecto/s",
-          });
-        }
-        //verificar que el usuario este activo en la plataforma
-        else if (results.estado === "inactivo") {
-          res.status(401).json({
-            error: true,
-            msg: "usuario inactivo",
-          });
-        }
-        //inicio de sesion exitoso
-        else {
-          const userName = results[0].nombres;
-          const lastName = results[0].apellidos;
-          const id = results[0].id;
-          const ciudad = results[0].ciudad;
-          //generacion del JWT
-          const token = jwt.sign(
-            { id: id, ciudad: ciudad },
-            process.env.SECRET_KEY,
-            {
-              expiresIn: process.env.JWT_EXPIRE,
-            }
-          );
-          //generacion de las cookies
-          const cookiesOptions = {
-            expires: new Date(
-              Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-            ),
-          };
-          res.cookie("jwt", token, cookiesOptions);
-          res.cookie("id", id, cookiesOptions);
-
-          res.status(200).json({
-            error: false,
-            msg: "inicio de sesión exitoso",
-            id: id,
-            usuario: userName + " " + lastName,
-            token,
-          });
-        }
-      }
-    );
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-//validar JWT
-exports.isAuthenticated = async (req, res, next) => {
-  if (req.cookies.jwt) {
-    try {
-      const decodificar = await promisify(jwt.verify)(
-        req.cookies.jwt,
-        process.env.SECRET_KEY
-      );
-      connection.query(
-        "SELECT * FROM users WHERE id = ?",
-        [decodificar.id],
-        (error, results) => {
-          if (!results) {
-            return next();
-          }
-          req.user = results[0];
-          return next();
-        }
-      );
-    } catch (error) {
-      res.status(400).json({
-        error: error.message,
-        msg: "token invalido",
-      });
-    }
-  } else {
-    res.status(401).json({
-      msg: "token inexsistente",
-    });
-  }
-};
-
-//obtener listado de departamentos
-exports.departamentos = async (req, res) => {
-  connection.query(
-    "SELECT * from departamentos",
-    function (error, results, fields) {
-      if (error) throw error;
-      res.status(200).json({
-        results,
-      });
-    }
-  );
-};
-//obtener listado de ciudades
-exports.ciudades = async (req, res) => {
-  const departamento = req.body.departamento;
-  connection.query(
-    "SELECT * FROM municipios WHERE departamento_id = ?",
-    [departamento],
-    function (error, results, fields) {
-      if (error) throw error;
-      res.status(200).json({
-        results,
-      });
-    }
-  );
-};
-//obtener listado de medicos
-exports.medicos = async (req, res) => {
-  connection.query("SELECT * from medico", function (error, results, fields) {
-    if (error) throw error;
-    res.status(200).json({
-      results,
-    });
-  });
 };
