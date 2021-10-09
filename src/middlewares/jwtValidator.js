b01b526f8c1c7f3f6f412562b44f76b3384ae746
Anderson <ander.er985@gmail.com>
2021-10-09 11:49:03 -0500

    roles implemented

--- src/middlewares/jwtValidator.js
+++ src/middlewares/jwtValidator.js
@@ -55,3 +55,101 @@ exports.adminRole = async (req, res, next) => {
     });
   }
 };
+
+exports.titularRole = async (req, res, next) => {
+  let token = req.headers["authorization"];
+  if (token) {
+    token = token.split(" ")[1];
+    try {
+      const decodificar = await promisify(jwt.verify)(
+        token,
+        process.env.SECRET_KEY
+      );
+
+      if (decodificar.tipo_usuario == 2 || decodificar.tipo_usuario == 1) {
+        console.log("usuario permitido");
+        next();
+      } else {
+        res.status(401).json({
+          error: "unauthorized user",
+          msg: "usuario no autorizado",
+        });
+      }
+    } catch (error) {
+      res.status(400).json({
+        error: error.message,
+        msg: "token invalido",
+      });
+    }
+  } else {
+    res.status(401).json({
+      msg: "token inexsistente",
+    });
+  }
+};
+exports.beneficiaryRole = async (req, res, next) => {
+  let token = req.headers["authorization"];
+  if (token) {
+    token = token.split(" ")[1];
+    try {
+      const decodificar = await promisify(jwt.verify)(
+        token,
+        process.env.SECRET_KEY
+      );
+
+      if (
+        decodificar.tipo_usuario == 3 ||
+        decodificar.tipo_usuario == 2 ||
+        decodificar.tipo_usuario == 1
+      ) {
+        console.log("usuario permitido");
+        next();
+      } else {
+        res.status(401).json({
+          error: "unauthorized user",
+          msg: "usuario no autorizado",
+        });
+      }
+    } catch (error) {
+      res.status(400).json({
+        error: error.message,
+        msg: "token invalido",
+      });
+    }
+  } else {
+    res.status(401).json({
+      msg: "token inexsistente",
+    });
+  }
+};
+exports.doctorRole = async (req, res, next) => {
+  let token = req.headers["authorization"];
+  if (token) {
+    token = token.split(" ")[1];
+    try {
+      const decodificar = await promisify(jwt.verify)(
+        token,
+        process.env.SECRET_KEY
+      );
+
+      if (decodificar.tipo_usuario == 4 || decodificar.tipo_usuario == 1) {
+        console.log("usuario permitido");
+        next();
+      } else {
+        res.status(401).json({
+          error: "unauthorized user",
+          msg: "usuario no autorizado",
+        });
+      }
+    } catch (error) {
+      res.status(400).json({
+        error: error.message,
+        msg: "token invalido",
+      });
+    }
+  } else {
+    res.status(401).json({
+      msg: "token inexsistente",
+    });
+  }
+};
--- src/routes/admin/admin.routes.js
+++ src/routes/admin/admin.routes.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const { aceptar, rechazar, solicitudes } = require("./adminController");
+const { adminRole } = require("../../middlewares/jwtValidator");
 const router = Router();
 
 //listado de solicitudes para ser medico
-router.get("/solicitud", solicitudes);
+router.get("/solicitud", adminRole, solicitudes);
 //rechazar solicitud
-router.post("/rechazar", rechazar);
+router.post("/rechazar", adminRole, rechazar);
 //aprobar solicitud
-router.post("/aprobar", aceptar);
+router.post("/aprobar", adminRole, aceptar);
 
 module.exports = router;
